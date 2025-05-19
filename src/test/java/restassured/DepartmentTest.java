ddb01e9fdfedc8215fdd46ac5c0a7d5baef646a1
azhar <mazharaa32@gmail.com>
2025-05-20 01:04:36 +0700

    Add getAllDepartmentTest Method

---
+++ src/test/java/restassured/DepartmentTest.java
@@ -0,0 +1,32 @@
+package restassured;
+
+import java.util.List;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class DepartmentTest extends BaseTest{
+    @Test(priority = 8)
+    public void getAllDepartmentTest() {
+        System.out.println("getAllDepartmentTest starting...");
+        // Get all department list request
+        Response actRes = RestAssured
+            .given()
+            .contentType("application/json")
+            .header("Authorization", "Bearer "+ token)
+            .log().all()
+            .when()
+            .get("api/department");
+
+        System.out.println(actRes.prettyPrint());
+        
+        List<Map<String, Object>> responseList = actRes.jsonPath().getList("");
+
+        Assert.assertEquals(actRes.statusCode(), 200);
+        Assert.assertFalse(responseList.isEmpty(), "Object list should not be empty");
+    }
+}
